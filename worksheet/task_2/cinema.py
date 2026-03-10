7da3ce15e217f73d6e38c7482435096dffae7bcf
d4nbrn <vmzr0073@leeds.ac.uk>
2026-03-10 16:52:01 +0000

    NEAR END OF TASK 2

--- worksheet/task_2/cinema.py
+++ worksheet/task_2/cinema.py
@@ -9,6 +9,14 @@ Please do not add any additional code underneath these functions.
 
 import sqlite3
 
+"""
+customers(**customer_id**, customer_name)
+films(**film_id**, title, age_rating)
+screenings(**screening_id**, film_id, screen)
+tickets(**ticket_id**, screening_id, customer_id, price)
+"""
+
+
 
 def customer_tickets(conn, customer_id):
     """
@@ -17,23 +25,74 @@ def customer_tickets(conn, customer_id):
 
     Include only tickets purchased by the given customer_id.
     Order results by film title alphabetically.
+
+    Write a function that returns details of tickets purchased by a specific customer.
+
+    The function should return a list of tuples containing (in order):
+    - the film title
+    - the screen
+    - the ticket price
+
+    Results should be ordered alphabetically by film title.
     """
+    cursor = conn.cursor()
+    query = """SELECT films.title, screenings.screen, tickets.price 
+    FROM customers JOIN tickets ON customers.customer_id = tickets.customer_id
+    JOIN screenings ON tickets.screening_id = screenings.screening_id
+    JOIN films ON screenings.film_id = films.film_id
+    WHERE customers.customer_id = ?
+    ORDER BY films.title ASC"""
+
+    cursor.execute(query,(customer_id,))
+
+    tuples = cursor.fetchall()
+    return tuples
     pass
 
 
 def screening_sales(conn):
     """
+    customers(**customer_id**, customer_name)
+    films(**film_id**, title, age_rating)
+    screenings(**screening_id**, film_id, screen)
+    tickets(**ticket_id**, screening_id, customer_id, price)
+
     Return a list of tuples:
     (screening_id, film_title, tickets_sold)
 
     Include all screenings, even if tickets_sold is 0.
     Order results by tickets_sold descending.
+    Write a function that returns the number of tickets sold for each screening.
+
+    The function should return a list of tuples containing (in order):
+    - the screening ID
+    - the film title
+    - the number of tickets sold
+
+    All screenings should be included, even if no tickets were sold for that screening.
+
+    Results should be ordered by the number of tickets sold, from highest to lowest.
     """
+    cursor = conn.cursor()
+    query = """SELECT screenings.screening_id, films.title, COUNT(tickets.screening_id)
+    FROM screenings INNER JOIN films ON screenings.film_id = films.film_id
+    LEFT JOIN tickets ON screenings.screening_id = tickets.screening_id
+    GROUP BY screenings.screening_id, films.title
+    ORDER BY COUNT(tickets.screening_id) DESC"""
+
+    cursor.execute(query)
+    tuples = cursor.fetchall()
+    return tuples
+
     pass
 
 
 def top_customers_by_spend(conn, limit):
     """
+    customers(**customer_id**, customer_name)
+    films(**film_id**, title, age_rating)
+    screenings(**screening_id**, film_id, screen)
+    tickets(**ticket_id**, screening_id, customer_id, price)
     Return a list of tuples:
     (customer_name, total_spent)
 
@@ -41,5 +100,21 @@ def top_customers_by_spend(conn, limit):
     Only include customers who have bought at least one ticket.
     Order by total_spent descending.
     Limit the number of rows returned to `limit`.
+    Write a function that returns the customers who have spent the most money on tickets.
+
+    The function should return a list of tuples containing (in order):
+    - the customer name
+    - the total amount spent on tickets
+
+    Only customers who have purchased at least one ticket should be included.
+
+    Results should be ordered by total amount spent, from highest to lowest, and limited to a specified number of rows (passed in by the argument `limit`)
     """
+
+    conn.cursor()
+    query = """SELECT customers.customer_name, tickets.price * SUM(tickets.ticket_id)
+    FROM customers INNER JOIN tickets ON customers.customer_id = tickets.customer_id
+    GROUP BY customers.customer_name, tickets.tickets.price
+
+
     pass
